e863203186aaf8a885456a445732258c607f46d0
codebrother <codebrother.org@gmail.com>
2016-12-25 13:35:13 +0800

    clear code

--- src/org/codebrother/jdbc/interfaces/ConnectionLib.java
+++ src/org/codebrother/jdbc/interfaces/ConnectionLib.java
@@ -1,9 +1,6 @@
 package org.codebrother.jdbc.interfaces;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import org.codebrother.jdbc.entity.DBSource;
 
